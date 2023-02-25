ef9c7365ceac1611d13077a4e87cdd8274a49d92
bharatrai53 <bharatrai2016@gmail.com>
2023-02-24 22:52:42 -0800

    fixed scripts

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.set("strictQuery", false);
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-  
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Failed to connect to MongoDB', err));
+});
 
 module.exports = mongoose.connection;
